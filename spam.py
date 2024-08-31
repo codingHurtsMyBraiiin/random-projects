9161101b1610b964a01e8459e77a5c0aaf1615d2
codingHurtsMyBraiiin <157146222+codingHurtsMyBraiiin@users.noreply.github.com>
2024-08-31 23:15:34 +0600

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,37 @@
+import pyautogui, time, datetime
+
+while True:
+    print(datetime.datetime.now())
+    pyautogui.write("haha")
+    pyautogui.press("enter")
+    time.sleep(0.1)
+    
+    print(datetime)
+
+
+
+'''
+haha
+hahahaha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+haha
+'''
